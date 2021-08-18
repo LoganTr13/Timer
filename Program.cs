b9ad772d08d862bbb02a504923abf623a8642c34
LoganTr13 <logaxgamer@gmail.com>
2021-08-18 13:00:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace Timer
+{
+    class Program
+    {        
+        static void Main()
+        {
+            string time = DateTime.Now.ToString("hh:mm");
+            Console.WriteLine("Defina o Horario que irá desligar (hh:mm): ");
+            string timer = Console.ReadLine();
+            while (true)
+            {
+                
+                time = DateTime.Now.ToString("hh:mm");
+                Console.WriteLine(DateTime.Now.ToString("hh:mm:ss"));
+                if(timer == time)
+                {
+                    break;
+                }
+            }
+            
+            Process.Start("shutdown.exe","-s -t 180 -c'Falta 30 mins para desligar, salve tudo!!'");
+            Console.WriteLine("acabou!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,4 @@
+### O Codigo posto aqui foi um teste
+
+- O Teste foi executado para eu evitar de mexer em uma aplicação minha e dar bugs.
+- Mas da para usar caso vá deixar o PC ligado e ir dormir.
